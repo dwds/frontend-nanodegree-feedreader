dbbc22216ef776fe6b61df8034aa5cbf1e5e17b5
Derek <derek@dadian-smith.net>
2016-11-23 22:31:03 -0600

    Create 'New Feed Selection' suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -72,7 +72,7 @@ $(function() {
       /* Ensures when the loadFeed function is called and completes its work,
        * there is at least a single .entry element within the .feed container.
        */
-       
+
       // first argument in $.contains must be DOM element, not jQuery object
       var feed = document.getElementsByClassName('feed')[0];
 
@@ -88,10 +88,12 @@ $(function() {
         done();
       });
     });
-    /* TODO: Write a new test suite named "New Feed Selection"
+
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
